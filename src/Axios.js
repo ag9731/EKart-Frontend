f4e543fb9500930a51de9412c3421cbd06e1ae25
ag9731 <goravanakollakash@gmail.com>
2026-02-19 18:36:34 +0530

    changes made in ofiice sysytem

--- src/Axios.js
+++ src/Axios.js
@@ -4,6 +4,7 @@ const api = axios.create({
   baseURL: "http://localhost:5000/api/v1/",
   timeout: 5000,
   headers: { "X-Custom-Header": "foobar" },
+  withCredentials: true
 });
 
 export default api;
